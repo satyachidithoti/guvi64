ada1eb79aef06a48106a41ed59ef579cc9879f3a
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-23 05:50:15 +0100

    Create 77.java

---
+++ 77.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();int cnt=0;
+		for(int i=1;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+		System.out.print(n);
+	}
+}
